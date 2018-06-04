19353cc6d63eea747732cf159249371d90998b8b
Pablo Schonwiesner <pabloschonwiesner@hotmail.com>
2018-06-04 12:16:16 -0300

    Commit final con agregado para hacker

--- js/pixel-art.js
+++ js/pixel-art.js
@@ -24,12 +24,120 @@ var nombreColores = ['White', 'LightYellow',
 // Es decir, el que se elige con la rueda de color.
 var colorPersonalizado = document.getElementById('color-personalizado');
 
+var paleta = $('#paleta');
+var grilla = $('#grilla-pixeles');
+var indicadorDeColor = $('#indicador-de-color')[0];
+var colorSeleccionado = '';
+var botonPresionado = false;
+var gomaActiva = false;
+var botonBorrar = $('#borrar');
+var botonGuardar = $('#guardar');
+var imgBatman = $('#batman');
+var imgWonder = $('#wonder');
+var imgFlash = $('#flash');
+var imgInvisible = $('#invisible');
+var lapiz = $('#lapiz');
+var goma = $('#goma');
+
+
+crearColores();
+crearGrilla();
+
 colorPersonalizado.addEventListener('change', 
   (function() {
     // Se guarda el color de la rueda en colorActual
-    colorActual = colorPersonalizado.value;
+    colorSeleccionado = colorPersonalizado.value;
     // Completar para que cambie el indicador-de-color al colorActual
-
-
+    mostrarColorSeleccionado();
   })
 );
+
+botonBorrar.click(borrarDibujoGrilla);
+botonGuardar.click(guardarPixelArt);
+imgBatman.click(function() {cargarSuperheroe(batman)});
+imgWonder.click(function() {cargarSuperheroe(wonder)});
+imgFlash.click(function() {cargarSuperheroe(flash)});
+imgInvisible.click(function() {cargarSuperheroe(invisible)});
+lapiz.click(activarLapiz);
+goma.click(activarGoma);
+
+function crearColores () {
+  nombreColores.forEach(function(item) {
+    var div = document.createElement('div');
+    div.className = 'color-paleta';
+    div.style.backgroundColor = item;
+    paleta.append(div);
+  });
+};
+
+function crearGrilla () {
+  for(var i = 0; i < 1750; i++) {
+    var div = document.createElement('div');
+    div.className = 'pixel';
+    grilla.append(div);
+  }
+}
+
+function seleccionarColor () {
+  colorSeleccionado = $(this)[0].style.backgroundColor;
+  mostrarColorSeleccionado();
+}
+
+function mostrarColorSeleccionado () {
+  indicadorDeColor.style.backgroundColor = colorSeleccionado;
+}
+
+function pintarPixel (pix) {
+    if(!gomaActiva) {
+      pix.style.backgroundColor = colorSeleccionado;
+    } else {
+      pix.style.backgroundColor = 'white';
+    }
+}
+
+function clickPixel () {
+  pintarPixel(this);
+}
+
+function overPixel () {
+  if(botonPresionado) {
+    pintarPixel(this);
+  }
+}
+
+function fnBotonPresionado () {
+  botonPresionado = true;
+}
+
+function fnBotonLiberado () {
+  botonPresionado = false;
+}
+
+function borrarDibujoGrilla () {
+  pixel.animate({"background-color": "white"}, 1000);
+}
+
+function activarLapiz () {
+  gomaActiva = false;
+  goma.removeClass('utilActivo');
+  lapiz.addClass('utilActivo');
+  grilla.removeClass('cursor-personalizado-goma');
+  grilla.addClass('cursor-personalizado-lapiz');
+}
+
+function activarGoma () {
+  gomaActiva = true;
+  lapiz.removeClass('utilActivo');
+  goma.addClass('utilActivo');
+  grilla.removeClass('cursor-personalizado-lapiz');
+  grilla.addClass('cursor-personalizado-goma');
+}
+
+$('.color-paleta').click(seleccionarColor);
+grilla.mousedown(fnBotonPresionado);
+grilla.mouseup(fnBotonLiberado);
+grilla.mouseleave(fnBotonLiberado);
+
+var pixel = $('.pixel');
+pixel.mouseover(overPixel);
+pixel.click(clickPixel);
